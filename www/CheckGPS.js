82dbaa48500abafa934cc69582745ce85bc16d76
juanHid <joan.h.iaweb@gmail.com>
2015-06-19 14:46:05 +0200

    Add open function

--- www/CheckGPS.js
+++ www/CheckGPS.js
@@ -3,3 +3,9 @@ exports.check=function(success, error, options){
 		options.alert = false || options.alert;
 		cordova.exec(success, error, "CheckGPS", "check", [options.alert]);
 };
+
+exports.open=function(success, error, options){
+		options = options || {};
+		options.alert = false || options.alert;
+		cordova.exec(success, error, "CheckGPS", "open", [options.alert]);
+};
