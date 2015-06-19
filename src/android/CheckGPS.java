afaaf99c1e87ffd22186f20b59a3389a6dfc9a1c
juanHid <joan.h.iaweb@gmail.com>
2015-06-19 14:52:40 +0200

    Add open

--- src/android/CheckGPS.java
+++ src/android/CheckGPS.java
@@ -9,6 +9,10 @@ import org.json.JSONObject;
 
 import android.location.LocationManager;
 import android.content.Context;
+
+import android.content.Intent;
+import android.provider.Settings;
+
 /*
  * thx to http://stackoverflow.com/questions/843675/how-do-i-find-out-if-the-gps-of-an-android-device-is-enabled
  */
@@ -19,16 +23,31 @@ public class CheckGPS extends CordovaPlugin{
 			this.check(callbackContext);
 			return true;
 		}
+		if (action.equals("open")) {
+			this.open(callbackContext);
+			return true;
+		}
 		return false;
 	}
 
 	private void check(CallbackContext callbackContext){
 		Context context = this.cordova.getActivity().getApplicationContext();
-    final LocationManager manager = (LocationManager) context.getSystemService( Context.LOCATION_SERVICE );
-    if ( manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+		final LocationManager manager = (LocationManager) context.getSystemService( Context.LOCATION_SERVICE );
+		if ( manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
 			callbackContext.success();
-    }else{
+		}else{
 			callbackContext.error(0);
 		}
-  }
+	}
+	
+	 private void open(CallbackContext callbackContext) {
+		try {
+			this.cordova.getActivity().startActivityForResult(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);		
+			callbackContext.success();
+		}
+		catch (Exception e){
+			callbackContext.error(0);
+		}
+	} 
+  
 }
